39e4893cd7517d09dbbc830a1f8c0a36b98db277
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-10-22 23:55:43 +0700

    Feat: Enhance password hashing utilities with panic recovery for safer execution

--- models/auth.go
+++ models/auth.go
@@ -6,10 +6,22 @@ import (
 )
 
 func HashPassword(password string) string {
+	defer func() {
+		if r := recover(); r != nil {
+			panic(r)
+		}
+	}()
+
 	hash := md5.Sum([]byte(password))
 	return hex.EncodeToString(hash[:])
 }
 
 func VerifyPassword(hashedPassword, inputPassword string) bool {
+	defer func() {
+		if r := recover(); r != nil {
+			panic(r)
+		}
+	}()
+
 	return hashedPassword == HashPassword(inputPassword)
 }
