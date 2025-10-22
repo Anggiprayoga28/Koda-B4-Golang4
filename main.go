17e2e9f08fc5717ad24f57e4fd191976d86e1ce5
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-10-22 17:44:38 +0700

    Refactor: Improve CLI structure and add user management system with pointers

--- main.go
+++ main.go
@@ -1,41 +1,54 @@
 package main
 
 import (
+	"Golang4/handlers"
+	"Golang4/models"
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
+var scanner *bufio.Scanner
+
 func main() {
-	defer fmt.Println("Selesai")
-	defer fmt.Println("Terima kasih")
+	defer fmt.Println("\nThank you for using our system!")
 
-	scanner := bufio.NewScanner(os.Stdin)
-
-	fmt.Println("~~~~~SELAMAT DATANG~~~~~")
+	scanner = bufio.NewScanner(os.Stdin)
+	var users []*models.User
 
 	for {
-		fmt.Println("\n1. Register")
-		fmt.Println("2. Login")
-		fmt.Println("3. Lupa Password")
-		fmt.Println("4. Keluar")
-		fmt.Print("\nPilihan: ")
+		showMainMenu()
+		choice := getInput("Choose a menu: ")
 
-		scanner.Scan()
-		pilihan := scanner.Text()
-
-		switch pilihan {
+		switch choice {
 		case "1":
-			fmt.Println("Menu Register")
+			handlers.RegisterUser(&users, scanner)
 		case "2":
 			fmt.Println("Menu Login")
 		case "3":
 			fmt.Println("Menu Lupa Password")
-		case "4":
-			fmt.Println("Keluar dari program")
+		case "0":
+			fmt.Println("\nGoodbye!")
 			os.Exit(0)
 		default:
-			fmt.Println("Pilihan salah")
+			fmt.Println("Invalid choice!")
 		}
 	}
+}
+
+func showMainMenu() {
+	defer fmt.Println()
+
+	fmt.Println("\n---- Welcome to system ----")
+	fmt.Println("\n1. Register")
+	fmt.Println("2. Login")
+	fmt.Println("3. Forgot Password")
+	fmt.Println("\n0. Exit")
+}
+
+func getInput(prompt string) string {
+	fmt.Print(prompt)
+	scanner.Scan()
+	return strings.TrimSpace(scanner.Text())
 }
